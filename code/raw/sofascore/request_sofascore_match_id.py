7baa03574ade30311ee3b19838b44b4a781a279b
SMNJSPHHRV <simon.herve@edu.devinci.fr>
2025-03-30 15:17:57 +0200

    add comments

--- code/raw/sofascore/request_sofascore_match_id.py
+++ code/raw/sofascore/request_sofascore_match_id.py
@@ -4,6 +4,7 @@ import pandas as pd
 import sys
 
 def insert_data(league_id, season_id, round, slug="", playoff=False):
+    # Make a request in function of the phase, regular or playoff
     if playoff:
         id_response = requests.get(f'https://www.sofascore.com/api/v1/unique-tournament/{league_id}/season/{season_id}/events/round/{round}/slug/{slug}')
     else:
@@ -13,6 +14,7 @@ def insert_data(league_id, season_id, round, slug="", playoff=False):
         data_id = id_response.json()
         matchs = []
         for _ in data_id["events"]:
+            # The data schema of the conserved data
             match = {
                 "id" : _["id"],
                 "tournament": _["tournament"]["slug"],
@@ -25,7 +27,7 @@ def insert_data(league_id, season_id, round, slug="", playoff=False):
             }
             matchs.append(match)
             del match
-        
+        # Save the data in the .csv file
         save_file = pd.read_csv(f"../../../data/raw/sofascore/id/matchs/{league_id}_{season_id}.csv") 
         data_to_save = pd.DataFrame(matchs)
         save_file = pd.concat([save_file, data_to_save])
@@ -36,9 +38,12 @@ def insert_data(league_id, season_id, round, slug="", playoff=False):
 
 
 def main(league_id, season_id):
+    # Init save_file dataframe
     save_file = pd.DataFrame(columns=["id", "tournament", "match_name", "league", "country", "season", "round", "status"])
     save_file.to_csv(f"../../../data/raw/sofascore/id/matchs/{league_id}_{season_id}.csv", index=False)
+    # Request the number of rounds and playoff
     rounds = requests.get(f"https://www.sofascore.com/api/v1/unique-tournament/{league_id}/season/{season_id}/rounds")
+    # Insert the data, it depend of the phase
     if rounds.status_code == 200:
         data_rounds = rounds.json()
         for _ in data_rounds["rounds"]:
@@ -50,6 +55,7 @@ def main(league_id, season_id):
         print("error")
 
 if __name__ == "__main__":
+    # able to execute the script with command line
     if len(sys.argv) != 3:
         print("Usage: python run.py <league_id> <season_id>")
         sys.exit(1)
